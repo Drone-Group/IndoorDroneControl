9bb04bc7238f0ba6e771ca6e7d97c828ba187925
Richmond Aisabor <richmondaisabor@Richmonds-Air.lan>
2021-03-27 00:48:22 -0500

    bash scripting

--- convert_position.py
+++ convert_position.py
@@ -4,7 +4,7 @@ from pyproj import Transformer
 def localize(pointX, pointY, pointZ):
 
 #Coordinate Reference System
-    inputEPSG = 4978
+    inputEPSG = 6584
     outputEPSG = 4326
     
 #Create an instance of the Transformer 
@@ -16,4 +16,5 @@ def localize(pointX, pointY, pointZ):
     return geoCoordinate[0], geoCoordinate[1], geoCoordinate[2]
 
 #Output
+
 print(localize(sys.argv[1], sys.argv[2], sys.argv[3]))
